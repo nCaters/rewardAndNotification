21a6aa31678657a1a8b33b4b0be0521489ab566a
Qian Jie <qianjie.qian@ncs.com.sg>
2023-03-24 14:07:49 +0800

    created client folder with create react app and created server folder

---
+++ server/server.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+
+const db = require('./db');
+
+// For middle wares
+app.use(morgan('dev'));
+
+// app.use(cors());
+app.use(express.json());
+
+const port = process.env.PORT || 3001;
+
+// testing
+app.get('/api/v1/test', async (req, res) => {
+  try {
+    //const results = await db.query("select * from restaurants");
+    const test = await db.query('select * from cuisine');
+
+    res.status(200).json({
+      status: 'success',
+      results: test.rows.length,
+      data: {
+        restaurants: test.rows,
+      },
+    });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`server is up and listening on  port ${port}`);
+});
