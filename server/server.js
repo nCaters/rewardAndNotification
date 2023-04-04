f61315e2c50920879b282b2185115c39b22d592f
Qian Jie <qianjie.qian@ncs.com.sg>
2023-04-04 09:35:46 +0800

    added jwt

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
+const jwt = require('jsonwebtoken');
 
 const db = require('./db');
 
@@ -30,6 +31,53 @@ app.get('/api/v1/test', async (req, res) => {
     console.log(err);
   }
 });
+
+app.post('/api/v1/login', async (req, res) => {
+  const { username, password } = req.body;
+
+  const user = await db.query(
+    `select * from "user" where username = $1 and password = $2`,
+    [username, password]
+  );
+
+  if (user) {
+    const data = user.rows[0];
+    // Generate an access token
+    const accessToken = jwt.sign(data, process.env.SECRET_ACCESS_TOKEN);
+    // res.json({
+    //   status: 'success',
+    //   results: user.rows.length,
+    //   data: {
+    //     user: user.rows,
+    //   },
+    // });
+    res.json({
+      accessToken,
+    });
+  } else {
+    res.status(400).json('Username or Password is incorrect!');
+  }
+
+  console.log(user);
+});
+
+const verify = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (authHeader) {
+    const token = authHeader.split(' ')[1];
+
+    jwt.verify(token, process.env.SECRET_ACCESS_TOKEN, (err, user) => {
+      if (err) {
+        return res.status(403).json('Token is not valid!');
+      }
+
+      req.user = user;
+      next();
+    });
+  } else {
+    res.status(401).json('You are not authenticated!');
+  }
+};
 
 // Get food
 app.get('/api/v1/food', async (req, res) => {
