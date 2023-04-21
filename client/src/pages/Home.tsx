6386278ed8fffc0a8cef3f1f1b6dda174396571f
Tania Tan <tania.tan@ncs.com.sg>
2023-04-21 16:23:34 +0800

    notification announcement bar based on date

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,9 +1,42 @@
+import { useEffect, useState } from "react";
+import '../css/Notification.css';
+
+
 const Home = () => {
     const logo = require('./ncatersicon.PNG');
+
+    const [notification, setNotification] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:3003/api/v1/notification')
+          .then(response => response.json())
+          .then(data => setNotification(data.data.message))
+          .catch(error => console.error(error));
+      }, []);
+    
+      const renderData = () => {
+        return (
+          <body>
+            {notification.length > 0 && <h3>Announcements:</h3> }
+            {notification.length > 0 && notification.map((item: any) => {
+              return (
+                  <td>{item.message}</td>
+              );
+            })}
+          </body>
+        );
+      };
+
     return (
-        <><h1>
-            Welcome to nCaterS
-        </h1><img src={logo} width="400" height="320"/></>
+        <>
+            <h1>
+                Welcome to nCaterS
+            </h1>
+            <body id="notification-message">
+                {renderData()}
+            </body>
+            <img src={logo} width="400" height="320"/>
+        </>
     );
 };
 
---
+++ notificationService/server.js
@@ -0,0 +1,48 @@
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+const jwt = require('jsonwebtoken');
+
+const ROLE = require('./common/enums/roles');
+
+const db = require('./db');
+
+// For middlewares
+app.use(morgan('dev'));
+
+// app.use(cors());
+app.use(express.json());
+
+const port = 3003;
+
+// Functions
+const generateAccessToken = (user) => {
+  return jwt.sign(user, process.env.SECRET_ACCESS_TOKEN, {
+    expiresIn: '30s',
+  });
+};
+
+const generateRefreshToken = (user) => {
+  return jwt.sign(user, process.env.SECRET_REFRESH_TOKEN);
+};
+
+app.get('/api/v1/notification', async (req, res) => {
+  try {
+    const result = await db.query('select * from notification where current_date = date');
+
+    res.status(200).json({
+      status: 'success',
+      results: result.rows.length,
+      data: {
+        message: result.rows,
+      },
+    });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`server is up and listening on  port ${port}`);
+});
